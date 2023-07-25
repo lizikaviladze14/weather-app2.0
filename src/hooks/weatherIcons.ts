1d3d8f580bae7800e62e4f28da17e8d491d44ea8
lizikaviladze14 <lizikavila@gmail.com>
2023-07-25 15:44:17 +0400

    removing garbage code which i write for garbage github pages

--- src/hooks/weatherIcons.ts
+++ src/hooks/weatherIcons.ts
@@ -1,50 +1,83 @@
-// const isLocal =
-//   window.location.hostname === "localhost" ||
-//   window.location.hostname === "127.0.0.1";
-
-// const iconsPath = isLocal ? "" : "weather-app2.0";
-
-const weatherIcons: { [key: string]: { [key: string]: string } } = {
-  "dark-mode": {
-    "01d": `weather-app2.0/images/weather_icons/dark-mode/icon_01d.png`,
-    "01n": `weather-app2.0/images/weather_icons/dark-mode/icon_01n.png`,
-    "02d": `weather-app2.0/images/weather_icons/dark-mode/icon_02d.png`,
-    "02n": `weather-app2.0/images/weather_icons/dark-mode/icon_02n.png`,
-    "03d": `weather-app2.0/images/weather_icons/dark-mode/icon_03d.png`,
-    "03n": `weather-app2.0/images/weather_icons/dark-mode/icon_03n.png`,
-    "04d": `weather-app2.0/images/weather_icons/dark-mode/icon_04d.png`,
-    "04n": `weather-app2.0/images/weather_icons/dark-mode/icon_04n.png`,
-    "09d": `weather-app2.0/images/weather_icons/dark-mode/icon_09d.png`,
-    "09n": `weather-app2.0/images/weather_icons/dark-mode/icon_09n.png`,
-    "10d": `weather-app2.0/images/weather_icons/dark-mode/icon_10d.png`,
-    "10n": `weather-app2.0/images/weather_icons/dark-mode/icon_10n.png`,
-    "11d": `weather-app2.0/images/weather_icons/dark-mode/icon_11d.png`,
-    "11n": `weather-app2.0/images/weather_icons/dark-mode/icon_11n.png`,
-    "13d": `weather-app2.0/images/weather_icons/dark-mode/icon_13d.png`,
-    "13n": `weather-app2.0/images/weather_icons/dark-mode/icon_13n.png`,
-    "50d": `weather-app2.0/images/weather_icons/dark-mode/icon_50d.png`,
-    "50n": `weather-app2.0/images/weather_icons/dark-mode/icon_50n.png`,
-  },
-  "light-mode": {
-    "01d": `weather-app2.0/images/weather_icons/light-mode/icon_01d.png`,
-    "01n": `weather-app2.0/images/weather_icons/light-mode/icon_01n.png`,
-    "02d": `weather-app2.0/images/weather_icons/light-mode/icon_02d.png`,
-    "02n": `weather-app2.0/images/weather_icons/light-mode/icon_02n.png`,
-    "03d": `weather-app2.0/images/weather_icons/light-mode/icon_03d.png`,
-    "03n": `weather-app2.0/images/weather_icons/light-mode/icon_03n.png`,
-    "04d": `weather-app2.0/images/weather_icons/light-mode/icon_04d.png`,
-    "04n": `weather-app2.0/images/weather_icons/light-mode/icon_04n.png`,
-    "09d": `weather-app2.0/images/weather_icons/light-mode/icon_09d.png`,
-    "09n": `weather-app2.0/images/weather_icons/light-mode/icon_09n.png`,
-    "10d": `weather-app2.0/images/weather_icons/light-mode/icon_10d.png`,
-    "10n": `weather-app2.0/images/weather_icons/light-mode/icon_10n.png`,
-    "11d": `weather-app2.0/images/weather_icons/light-mode/icon_11d.png`,
-    "11n": `weather-app2.0/images/weather_icons/light-mode/icon_11n.png`,
-    "13d": `weather-app2.0/images/weather_icons/light-mode/icon_13d.png`,
-    "13n": `weather-app2.0/images/weather_icons/light-mode/icon_13n.png`,
-    "50d": `weather-app2.0/images/weather_icons/light-mode/icon_50d.png`,
-    "50n": `weather-app2.0/images/weather_icons/light-mode/icon_50n.png`,
-  },
-};
-
-export default weatherIcons;
+// import icon_01d_dark from "images/weather_icons/dark-mode/icon_01d.png";
+// import icon_01n_dark from "images/weather_icons/dark-mode/icon_01n.png";
+// import icon_02d_dark from "images/weather_icons/dark-mode/icon_02d.png";
+// import icon_02n_dark from "images/weather_icons/dark-mode/icon_02n.png";
+// import icon_03d_dark from "images/weather_icons/dark-mode/icon_03d.png";
+// import icon_03n_dark from "images/weather_icons/dark-mode/icon_03n.png";
+// import icon_04d_dark from "images/weather_icons/dark-mode/icon_04d.png";
+// import icon_04n_dark from "images/weather_icons/dark-mode/icon_04n.png";
+// import icon_09d_dark from "images/weather_icons/dark-mode/icon_09d.png";
+// import icon_09n_dark from "images/weather_icons/dark-mode/icon_09n.png";
+// import icon_10d_dark from "images/weather_icons/dark-mode/icon_10d.png";
+// import icon_10n_dark from "images/weather_icons/dark-mode/icon_10n.png";
+// import icon_11d_dark from "images/weather_icons/dark-mode/icon_11d.png";
+// import icon_11n_dark from "images/weather_icons/dark-mode/icon_11n.png";
+// import icon_13d_dark from "images/weather_icons/dark-mode/icon_013d.png";
+// import icon_13n_dark from "images/weather_icons/dark-mode/icon_13n.png";
+// import icon_50d_dark from "images/weather_icons/dark-mode/icon_50d.png";
+// import icon_50n_dark from "images/weather_icons/dark-mode/icon_50n.png";
+//
+// import icon_01d_light from "images/weather_icons/light-mode/icon_01d.png";
+// import icon_01n_light from "images/weather_icons/light-mode/icon_01n.png";
+// import icon_02d_light from "images/weather_icons/light-mode/icon_02d.png";
+// import icon_02n_light from "images/weather_icons/light-mode/icon_02n.png";
+// import icon_03d_light from "images/weather_icons/light-mode/icon_03d.png";
+// import icon_03n_light from "images/weather_icons/light-mode/icon_03n.png";
+// import icon_04d_light from "images/weather_icons/light-mode/icon_04d.png";
+// import icon_04n_light from "images/weather_icons/light-mode/icon_04n.png";
+// import icon_09d_light from "images/weather_icons/light-mode/icon_09d.png";
+// import icon_09n_light from "images/weather_icons/light-mode/icon_09n.png";
+// import icon_10d_light from "images/weather_icons/light-mode/icon_10d.png";
+// import icon_10n_light from "images/weather_icons/light-mode/icon_10n.png";
+// import icon_11d_light from "images/weather_icons/light-mode/icon_11d.png";
+// import icon_11n_light from "images/weather_icons/light-mode/icon_11n.png";
+// import icon_13d_light from "images/weather_icons/light-mode/icon_013d.png";
+// import icon_13n_light from "images/weather_icons/light-mode/icon_13n.png";
+// import icon_50d_light from "images/weather_icons/light-mode/icon_50d.png";
+// import icon_50n_light from "images/weather_icons/light-mode/icon_50n.png";
+//
+// const weatherIcons: { [key: string]: string } = {
+//   icon_01d_dark,
+//   icon_01n_dark,
+//   icon_02d_dark,
+//   icon_02n_dark,
+//   icon_03d_dark,
+//   icon_03n_dark,
+//   icon_04d_dark,
+//   icon_04n_dark,
+//   icon_09d_dark,
+//   icon_09n_dark,
+//   icon_10d_dark,
+//   icon_10n_dark,
+//   icon_11d_dark,
+//   icon_11n_dark,
+//   icon_13d_dark,
+//   icon_13n_dark,
+//   icon_50d_dark,
+//   icon_50n_dark,
+//
+//   icon_01d_light,
+//   icon_01n_light,
+//   icon_02d_light,
+//   icon_02n_light,
+//   icon_03d_light,
+//   icon_03n_light,
+//   icon_04d_light,
+//   icon_04n_light,
+//   icon_09d_light,
+//   icon_09n_light,
+//   icon_10d_light,
+//   icon_10n_light,
+//   icon_11d_light,
+//   icon_11n_light,
+//   icon_13d_light,
+//   icon_13n_light,
+//   icon_50d_light,
+//   icon_50n_light,
+// };
+//
+// function getImageByKey(key: string) {
+//   return weatherIcons[key];
+// }
+//
+// export default getImageByKey;
